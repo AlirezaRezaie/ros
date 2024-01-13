31c6c8c108d638b8f90623c73a9ccb5a402e5be4
AlirezaRezaie <rezaiealireza884@gmail.com>
2024-01-13 21:58:18 +0330

    chore: function to get time stamp

--- src/arch/x86/utils/time.c
+++ src/arch/x86/utils/time.c
@@ -10,6 +10,26 @@ unsigned char rtc_read(unsigned char reg) {
     return inb(0x71);
 }
 
+int get_unix_time(){
+    unsigned long unix_time = 0;
+
+    int hours = (int) rtc_read(0x04);
+    int minutes = (int) rtc_read(0x02);
+    int seconds = (int) rtc_read(0x00);
+
+    int seconds_h = (seconds >> 4) & 0x0F;
+    int seconds_l = seconds & 0x0F;
+
+    int new = (seconds_h)*10 + seconds_l;
+
+    // Calculate Unix time by adding seconds for hours, minutes, and seconds
+    unix_time += hours * 3600; // Hours to seconds
+    unix_time += minutes * 60; // Minutes to seconds
+    unix_time += new; // Seconds
+
+    return unix_time;
+}
+
 void bcd_to_string(unsigned char reg,unsigned char* high,unsigned char* low){
     unsigned char bcd = rtc_read(reg);
     intToString((unsigned int)(bcd >> 4) & 0x0F, high);
@@ -41,4 +61,16 @@ void get_time(unsigned char* buffer){
     add_to_string(second_high,buffer,&buf_size);
     add_to_string(second_low,buffer,&buf_size);
 
+}
+
+int get_timestamp() {
+    int low, high;
+    asm volatile("rdtsc" : "=a" (low), "=d" (high));
+    return low;
+}
+
+int delay_passed(int* current){
+    // true if 2 seconds has passed since the current time pointer
+    if (get_unix_time() - *current < 2)return 0;
+    return 1;
 }
--- src/arch/x86/utils/time.h
+++ src/arch/x86/utils/time.h
@@ -2,5 +2,8 @@
 #define TIME_H
 
 void get_time(unsigned char* buffer);
+int get_unix_time();
+int delay_passed(int* current);
+int get_timestamp();
 
 #endif
