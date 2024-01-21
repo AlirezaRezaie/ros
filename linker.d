a808470fffde6583259a1218b901a6a782a488ab
AlirezaRezaie <rezaiealireza884@gmail.com>
2024-01-22 00:18:33 +0330

    fix: format linker script

--- linker.d
+++ linker.d
@@ -2,25 +2,20 @@ ENTRY(_start)
 
 SECTIONS
 {
-
-   /* this makes the kernel binary addresses relative to where it's loaded from bootloader which is address 0x1000
-   in short => all addresses (including jmp and etc.) will be relative to the 0x1000 to. */
-   
-   /* we can access custom symbols (memory location ) in c using the keyword extern 
-      and use it here.
-   */
-
    . = 0x1000;
 
    .text :
    {
       *(.text)
-
    }
 
-   
-   .data : { *(.data) }
+   .bss :
+   {
+      *(.bss)
+   }
 
-   .bss : { *(.bss) }
-
+   .data :
+   {
+      *(.data)
+   }
 }
